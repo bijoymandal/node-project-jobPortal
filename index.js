e2d060eb5b22108dfba48bfbd71c9fc6dbe4e848
bijoymandal <bijoy.mandal@mydevteam.com>
2025-09-26 18:25:14 +0530

    Add JobListController and define jobs route

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import path from "path";
 import { fileURLToPath } from "url";
 import ejsLayouts from "express-ejs-layouts";
 import IndexController from "./src/controllers/Index.Controller.js";
+import JobListController from "./src/controllers/joblist.controller.js";
 
 // Fix __dirname in ES Module
 const __filename = fileURLToPath(import.meta.url);
-console.log(__filename);
 const __dirname = path.dirname(__filename);
 
 // Create server
@@ -29,11 +29,13 @@ server.set("layout", "layouts/layout"); // uses src/views/layouts/layout.ejs
 
 // Instantiate Controller (if needed)
 const indexController = new IndexController();
+const jobListController = new JobListController();
 
 // Routes
 server.get("/", (req, res) => {
   res.render("landing-page", { title: "Home Page" });
 });
+server.get("/jobs",jobListController.index);
 
 
 // Start server

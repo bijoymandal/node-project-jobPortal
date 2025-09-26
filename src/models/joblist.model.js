80bfabb640ff9e697c940f5132d88fbd1cf456f6
bijoymandal <bijoy.mandal@mydevteam.com>
2025-09-26 18:25:32 +0530

    Add JobListModel class and initial job data

---
+++ src/models/joblist.model.js
@@ -0,0 +1,86 @@
+export default class JobListModel{
+    constructor(_id,_job_designation,_logo,_company_name,_job_location,_experience,_salary,_employees,_skills_required=[],_job_posted){
+        this.id = _id;
+        this.job_designation = _job_designation;
+        this.logo = _logo;
+        this.company_name = _company_name;
+        this.job_location = _job_location;
+        this.experience = _experience;
+        this.salary = _salary;
+        this.employees = _employees;
+        this.skills_required = _skills_required,
+        this.job_posted = _job_posted || new Date();
+    }
+
+    static getAllJobs() {
+        return JobModel;
+    }
+
+    static getJobById(id) {
+        return JobModel.find(job => job.id == id);
+    }
+}
+
+var JobModel =[
+    new JobListModel(
+        1,
+        "Senior Frontend Developer",
+        "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg",
+        "google",
+        "Gurgaon HR INR Remote",
+        "3-5 years",
+        "₹15-25 LPA",
+        "190,000+ employees",
+        ["HTML","CSS","JavaScript","ReactJS"],
+        new Date("2023-10-01")
+    ),
+    new JobListModel(
+        2,
+        "Angular Developer",
+        "https://upload.wikimedia.org/wikipedia/commons/c/cf/Angular_full_color_logo.svg",
+        "Facebook",
+        "Pune InD on Site",
+        "3 years",
+        "₹10-15 LPA",
+        "10,000+ employees",
+        ["Angular","Js","SQL","MongoDB","ExpressJS","AWS"],
+        new Date("2023-09-25")
+
+    ),
+    new JobListModel(
+        3,
+        "SDE",
+        "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg",
+        "Juspay",
+        "Banglore IND",
+        "6+ years",
+        "₹30-50 LPA",
+        "2,000+ employees",
+        ["React","NodeJs","Javascript","SQL","Mongo DB","Express","AWS"],
+        new Date("2023-09-20")
+    ),
+    new JobListModel(
+        4,
+        "Software Application Engineer II",
+        "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+        "Microsoft",
+        "Hyderabad IND on Site",
+        "5+ years",
+        "₹35-45 LPA",
+        "220,000+ employees",
+        ["C#",".Net","Azure","SQL","Microservices"],
+        new Date("2023-09-18")
+    ),
+    new JobListModel(
+        5,
+        "UI/UX Designer",
+        "https://upload.wikimedia.org/wikipedia/commons/a/ab/Apple-logo.png",
+        "Apple",
+        "Chennai IND on Site",
+        "4+ years",
+        "₹20-30 LPA",
+        "150,000+ employees",
+        ["Figma","Adobe XD","Sketch","InVision","HTML","CSS"],
+        new Date("2023-09-15")
+    )
+];

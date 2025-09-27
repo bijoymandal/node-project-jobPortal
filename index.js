29ac69b3f1c12ed39c6c507ae2c19a6415ebaff3
bijoymandal <bijoy.mandal@mydevteam.com>
2025-09-27 20:51:55 +0530

    Add session management and cookie parsing middleware

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import { fileURLToPath } from "url";
 import ejsLayouts from "express-ejs-layouts";
 import IndexController from "./src/controllers/Index.Controller.js";
 import JobListController from "./src/controllers/joblist.controller.js";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import { setLastVisit } from "./src/middleware/lastVisit.middleware.js";
 
 // Fix __dirname in ES Module
 const __filename = fileURLToPath(import.meta.url);
@@ -18,6 +21,20 @@ server.use(express.json());
 
 // Serve static files (CSS, JS, images)
 server.use(express.static(path.join(__dirname,"public")));
+
+//server session
+server.use(
+  session({
+    secret: "your-secret-key", // Replace with your secret key
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // Set to true if using HTTPS
+  }),
+);
+//cookie parser
+server.use(cookieParser());
+server.use(setLastVisit); // Use last visit middleware
+
 
 // View engine setup
 server.set("view engine", "ejs");
